4d134550ce5d32bcd8385e27e851517f46a53b3d
DbWang <w45241397@163.com>
2023-06-19 21:52:48 +0800

    feat: vue-cli配置代理解决跨域

---
+++ 18_vue_proxy/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="root">
+      <h2>hello</h2>
+      <button @click="getStudengInfo">获取学生信息</button><hr><br><br>
+      <button @click="getCarInfo">获取汽车信息</button>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    name: "App",
+    methods:{
+        getStudengInfo(){
+            // 方式1
+            // axios.get("http://localhost:8080/students").then(
+            // 方式2  前缀方式
+            axios.get("http://localhost:8080/proapi/students").then(
+                response =>{
+                    console.log('请求成功: ',response.data)
+                },
+                err =>{
+                    console.log("failed ",err)
+                }
+            )
+        },
+        getCarInfo(){
+            axios.get("http://localhost:8080/carapi/cars").then(
+                res => {
+                    console.log('success')
+                    console.log(res)
+                },
+                err =>{
+                    console.log(err)
+                }
+            )
+        }
+    }
+}   
+</script>
+
+
+
+
+
+
+
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,36 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+
+  // 方式1 配置代理，不够灵活，只能配置1个服务器
+  // devServer:{
+  //   proxy:'http://localhost:5000'
+  // }
+
+  // 方式2
+  devServer:{
+    proxy:{
+      '/proapi':{
+        target:'http://localhost:5000',
+        //重写路径
+        pathRewrite:{'^/proapi':''}
+
+        //支持ws
+        // ws:true,
+        //控制host值
+        // changeOrigin:true
+      },
+      '/carapi':{
+        target:'http://localhost:5001',
+        //重写路径
+        pathRewrite:{'^/carapi':''}
+
+        //支持ws
+        // ws:true,
+        //控制host值
+        // changeOrigin:true
+      }
+    }
+  }
 })
