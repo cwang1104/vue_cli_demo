6850f3c2c09ed16ce23892c9f5ae290899b7d57a
DbWang <w45241397@163.com>
2023-06-24 15:15:09 +0800

    feat: vuex_mapactions_mapmutations

--- 24_vuex_map_to_computed/components/Count.vue
+++ 24_vuex_map_to_computed/components/Count.vue
@@ -9,15 +9,15 @@
             <option value="2">2</option>
             <option value="3">3</option>
         </select>
-        <button @click="increment">+</button>
-        <button @click="decrement">-</button>
-        <button @click="incrementOdd">当前为奇数可加1</button>
-        <button @click="decrementWait">等一等再加1</button>
+        <button @click="increment(n)">+</button>
+        <button @click="decrement(n)">-</button>
+        <button @click="incrementOdd(n)">当前为奇数可加1</button>
+        <button @click="incrementWait(n)">等一等再加1</button>
     </div>
 </template>
 
 <script>
-import { mapState,mapGetters } from 'vuex'
+import { mapState,mapGetters,mapMutations,mapActions } from 'vuex'
 export default {
     name:"Count",
     data(){
@@ -42,19 +42,31 @@ export default {
         ...mapGetters(["bigSum"]),
     },
     methods:{
-        increment(){
-           this.$store.dispatch("sum",this.n)
-           console.log(this.$store)
-        },
-        decrement(){
-          this.$store.dispatch("de",this.n)
-        },
-        incrementOdd(){
-            this.$store.dispatch("sumOdd",this.n)
-        },
-        decrementWait(){
-            this.$store.dispatch("sumWait",this.n)
-        }
+
+        //借助mapMutations生成方法,均有数组写法，同上
+        ...mapMutations({increment:"SUM",decrement:"DE"}),
+        // ==>相当于：
+        // increment(val){
+        //     this.$store.commit("SUM",val)
+        // }
+
+        // increment(){
+        //    this.$store.dispatch("sum",this.n)
+        //    console.log(this.$store)
+        // },
+        // decrement(){
+        //   this.$store.dispatch("de",this.n)
+        // },
+
+
+        //mapActions
+            ...mapActions({incrementOdd:"sumOdd",incrementWait:"sumWait"}),
+        // incrementOdd(){
+        //     this.$store.dispatch("sumOdd",this.n)
+        // },
+        // decrementWait(){
+        //     this.$store.dispatch("sumWait",this.n)
+        // }
     }
     
 }
