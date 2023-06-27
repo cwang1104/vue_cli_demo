08555359ee1e185d7d84c6609c0e177ef09e6782
DbWang <w45241397@163.com>
2023-06-27 21:49:53 +0800

    feat: 路由传参

---
+++ 30_router_query_params/router/index.js
@@ -0,0 +1,42 @@
+//路由管理
+import {createRouter,createWebHashHistory} from 'vue-router'
+
+//引入组件
+import Home from '../pages/Home'
+import About from '../pages/About'
+import News from '../pages/News'
+import Message from '../pages/Message'
+import Detail from '../pages/Detail'
+
+const router = new createRouter({
+    routes:[
+        {
+            path:'/about',
+            component:About
+
+        },
+        {
+            path:'/home',
+            component:Home,
+            children:[
+                {
+                    path:'news',
+                    component:News
+                },
+                {
+                    path:'message',
+                    component:Message,
+                    children:[
+                        {
+                            path:'detail',
+                            component:Detail,
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    history:createWebHashHistory()
+})
+
+export default router
