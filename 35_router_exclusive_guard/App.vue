9014282ea293c58754f69b2078b4d0d05c134027
milo <w45241397@163.com>
2023-06-30 13:34:21 +0800

    feat: 独享路由守卫

---
+++ 35_router_exclusive_guard/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-xs-offset-2 col-xs-8">
+                <div class="page-header">
+                    <Header></Header>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-2 col-xs-offset-2">
+                <div class="list-group">
+
+                    <!-- 利用a标签切换页面 -->
+                    <!-- <a class="list-group-item" href="./about.html">About</a>
+                    <a class="list-group-item active" href="./home.html">Home</a> -->
+
+                    <!-- 利用router-link切换路由 -->
+                    <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
+                    <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
+                </div>
+            </div>
+            <div class="col-xs-6">
+                <div class="panel">
+                    <div class="panel-body">
+                        <router-view></router-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+import Header from './components/Header.vue'
+export default {
+    name: "App",
+    components:{Header}
+}   
+</script>
+
+
+
+
+
---
+++ 35_router_exclusive_guard/pages/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+ <ul>
+    <li>消息id: {{ id }}</li>
+    <li>消息标题：{{ title }} </li>
+ </ul>
+</template>
+
+<script>
+export default {
+    name:"Detail",
+    props:['title','id']    
+}
+</script>
+
+<script>
+</script>
