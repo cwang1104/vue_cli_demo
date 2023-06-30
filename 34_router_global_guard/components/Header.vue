f0aa43fa42cadb0153dd596596fd82878f9ea14b
milo <w45241397@163.com>
2023-06-30 13:27:23 +0800

    feat： 全局路由守卫

---
+++ 34_router_global_guard/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>Vue Router Demo</h2>
+    <button @click="back">后退</button>
+    <button @click="forward">前进</button>
+</template>
+
+<script>
+export default {
+    name:'Header',
+    methods:{
+        back(){
+            this.$router.back()
+        },
+        forward(){
+            this.$router.forward()
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
